ff269bc60d573b537ea7463ea2e7dcb962b23303
DevJasp <jasphergargar@gmail.com>
2025-08-29 15:02:02 +0800

    Change padding of the header

--- quality-control/src/components/layout/DashboardHeader.tsx
+++ quality-control/src/components/layout/DashboardHeader.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 
 const DashboardHeader: React.FC = () => {
   return (
-    <div className="text-center mb-8">
-      <h1 className="text-4xl font-bold text-gray-800 mb-2">
+    <div className=" mb-20 mt-6 max-w-xl text-center mx-auto">
+      <h1 className="text-4xl py-2  rounded-xl bg-green-200 font-bold text-gray-800 mb-2">
         Quality Control Dashboard
       </h1>
       <p className="text-gray-600">
--- quality-control/src/components/panels/QCRulesPanel.tsx
+++ quality-control/src/components/panels/QCRulesPanel.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import type { QCRulesPanelProps } from '../../types/qc.types';
 
 const QCRulesPanel: React.FC<QCRulesPanelProps> = ({ qcRules }) => {
